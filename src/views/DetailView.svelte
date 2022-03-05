bedbc71c7d3d3dad910ec264b55e6ec604537707
jeonghoonyu <moning02004@naver.com>
2022-03-05 20:16:36 +0900

    메시지 변경.

--- src/views/DetailView.svelte
+++ src/views/DetailView.svelte
@@ -4,7 +4,7 @@
     let detail = "";
     userStatus.subscribe(value => {
         if (value === "O") {
-            detail = "환영합니다."
+            detail = "환영합니다. 이제 회원 정보를 뿌려줄 수 있도록 모델 정보를 목업 데이터로 생성하자."
         } else {
             detail = ""
         }
